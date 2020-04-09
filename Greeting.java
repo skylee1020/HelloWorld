5177825eca945065b3c9e746144ba2706f815fe2
skylee1020 <45272842+skylee1020@users.noreply.github.com>
2020-04-09 22:38:17 +0900

    Add files via upload
    a test upload

---
+++ Greeting.java
@@ -0,0 +1,16 @@
+//********************************************************************
+//  Greeting.java
+//
+//  Demonstrates the basic structure of a Java application.
+//********************************************************************
+
+public class Greeting
+{
+   //-----------------------------------------------------------------
+   //  Prints a greeting message.
+   //-----------------------------------------------------------------
+   public static void main (String[] args)
+   {
+	System.out.println("Hello Kim!");
+   }
+}

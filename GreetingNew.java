026507959de96633ac10e30891be39adaf808d4b
skylee1020 <skylee1020@gmail.com>
2023-02-28 14:30:26 +0900

    First Commit

---
+++ GreetingNew.java
@@ -0,0 +1,22 @@
+//********************************************************************
+//  Greeting.java
+//
+//  Demonstrates the basic structure of a Java application.
+//********************************************************************
+
+public class Greeting
+{
+   //-----------------------------------------------------------------
+   //  Prints a greeting message.
+   //-----------------------------------------------------------------
+   public static void main (String[] args)
+   {
+	System.out.println("Hello Kim!");
+	System.out.println("Hello Lee!");
+
+	System.out.println("Hello Cho!");
+	System.out.println("Hello");   
+	System.out.println("Hello");   
+
+   }
+}
---
+++ readme.txt
@@ -0,0 +1,15 @@
+https://midas123.tistory.com/224
+	기본 용어 간략히 정리
+https://jong99.tistory.com/26
+
+https://docs.github.com/ko/get-started/quickstart/contributing-to-projects
+
+https://evan-moon.github.io/2019/07/25/git-tutorial/
+
+
+https://gitforwindows.org/
+https://desktop.github.com/
+	https://cofs.tistory.com/421
+
+https://git-scm.com/downloads
+	설치 https://coding-factory.tistory.com/245
